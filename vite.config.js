2740c57ed19be0d95a7baa77834d5c453d15131c
GsCabral <cabralgonzalo10@gmail.com>
2024-12-15 09:37:41 -0300

    cambio viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3007,
-    open: true,
-  }})
+  base: '/Proyectos-libros/', // Cambia "<nombre-del-repositorio>" por el nombre de tu repo
+})
